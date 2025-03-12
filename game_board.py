72f8c4a8f2e1ebce6bc508fc50b30003e8e1fada
owenHillary <owenlhillary@gmail.com>
2025-03-12 15:31:59 -0400

    Added comments to code

--- game_board.py
+++ game_board.py
@@ -8,13 +8,14 @@ class Board:
     def __init__(self, width, height):
         # List that will keep track of tile placement
         self.tile_list = ["O", "S", "Wo", "Wh", "B", "S", "B", "Wh", "Wo", "D",
-                     "Wo", "O", "Wo", "O", "Wh", "S", "B", "Wh", "S"
-                    ]
+                          "Wo", "O", "Wo", "O", "Wh", "S", "B", "Wh", "S"
+                          ]
 
         self.grid = grid.Graph()
 
         # Type checks inputs and Sets
-        if (isinstance(width, int) or isinstance(width, float)) and (isinstance(height, int) or isinstance(height, float)):
+        if (isinstance(width, int) or isinstance(width, float)) and (
+                isinstance(height, int) or isinstance(height, float)):
             self.height = height
             self.width = width
         else:
@@ -22,11 +23,11 @@ class Board:
 
         # The colors of each different material in a dictionary
         self.colors = {
-            "Wo": (81,125,25),
-            "B": (156,67,0),
-            "Wh": (240,173,0),
-            "O": (123,111,131),
-            "S": (143,206,0),
+            "Wo": (81, 125, 25),
+            "B": (156, 67, 0),
+            "Wh": (240, 173, 0),
+            "O": (123, 111, 131),
+            "S": (143, 206, 0),
             "D": (194, 178, 128)
         }
 
@@ -51,7 +52,7 @@ class Board:
     def __calculate_diamond(center_x, center_y, radius):
         points = []
         for i in range(4):  # 4 sides in a diamond
-            angle_deg = 90 * i- 45   # Start at -90° so that point is at the top
+            angle_deg = 90 * i - 45  # Start at -90° so that point is at the top
             angle_rad = math.radians(angle_deg)
             x = center_x + radius * math.cos(angle_rad)
             y = center_y + radius * math.sin(angle_rad)
@@ -69,11 +70,7 @@ class Board:
         self.start_y = self.height / 6  # Starting y pos of the grid
         self.hex_size = self.height / 10  # Size of the hexagon
         self.hex_diff = self.hex_size * 1.875  # Difference between hexagons horizontally
-        self.row_height = self.hex_diff - 21 # Difference between hexagons vertically
-
-
-
-
+        self.row_height = self.hex_diff - 21  # Difference between hexagons vertically
 
         # The index position for tiles and number lists
         tile_idx = 0
@@ -87,7 +84,6 @@ class Board:
             (4, 3),
             (3, 4)
         ]
-
 
         self.point_lst = []
         for hex_count, row_offset in rows:
@@ -103,7 +99,7 @@ class Board:
 
                 # If it is the desert tile, move the robber, don't add number.
                 if self.tile_list[tile_idx] == "D":
-                    self.robber_pos = (x,y)
+                    self.robber_pos = (x, y)
                 # Otherwise add the number like a normal tile
                 else:
                     # Initialize the number
@@ -128,7 +124,6 @@ class Board:
             pygame.draw.circle(screen, (255, 0, 0), self.robber_pos, int(self.hex_size * 0.4))
         pygame.draw.circle(screen, robber_color, self.robber_pos, int(self.hex_size * 0.3))
 
-
     # Shuffles the entire board and numbers
     def shuffle_board(self):
         random.shuffle(self.tile_list)
@@ -137,26 +132,20 @@ class Board:
     # Creates the beginner board
     def beginner_board(self):
         self.tile_list = ["O", "S", "Wo", "Wh", "B", "S", "B", "Wh", "Wo", "D",
-                     "Wo", "O", "Wo", "O", "Wh", "S", "B", "Wh", "S"
-                    ]
+                          "Wo", "O", "Wo", "O", "Wh", "S", "B", "Wh", "S"
+                          ]
         self.numbers = [10, 2, 9, 12, 6, 4, 10, 9, 11, 3, 8, 8, 3, 4, 5, 5, 6, 11]
 
         # Loop through each hex in each row
 
+    # Sets up the map of all buildings
     def create_buildings(self):
 
-        """# Builds all the exterior houses
-        order = [[0, 5], [0], [0, 1], [5], [1], [4, 5], [1, 2], [4], [2], [3, 4], [3], [2, 3]]
-        order_b = [0, 1, 2, 3, 6, 7, 11, 12, 15, 16, 17, 18]
-        count = -1
-        for i in order_b:
-            count += 1
-            for d in order[count]:
-                x = self.point_lst[i][d][0]
-                y = self.point_lst[i][d][1]
-                pygame.draw.polygon(screen, (100, 20, 50), self.__calculate_diamond(x, y, 10))"""
 
+        # Loops for every hex - Finds center location betweens each hec for nodes
         for i in range(len(self.point_lst)):
+
+            # Finds what row the top most node is in for each hex
             if i <= 2:
                 top_row = 0
                 node = i
@@ -173,47 +162,30 @@ class Board:
                 node = i - 16
                 top_row = 8
 
+            # Goes through every point in hex and sends cords to node
             if top_row < 6:
                 self.grid.node_list[top_row][node].location.append(self.point_lst[i][0])
-                print(f'hex {i} add {self.point_lst[i][0]} to {self.grid.node_list[top_row][node]}')
             else:
                 self.grid.node_list[top_row][node + 1].location.append(self.point_lst[i][0])
-                print(f'hex {i} add {self.point_lst[i][0]} to {self.grid.node_list[top_row][node + 1]}')
 
             self.grid.node_list[top_row + 1][node].location.append(self.point_lst[i][5])
-            print(f'hex {i} add {self.point_lst[i][5]} to {self.grid.node_list[top_row + 1][node]}')
-
             self.grid.node_list[top_row + 1][node + 1].location.append(self.point_lst[i][1])
-            print(f'hex {i} add {self.point_lst[i][1]} to {self.grid.node_list[top_row + 1][node + 1]}')
-
             self.grid.node_list[top_row + 2][node].location.append(self.point_lst[i][4])
-            print(f'hex {i} add {self.point_lst[i][4]} to {self.grid.node_list[top_row + 2][node]}')
-
             self.grid.node_list[top_row + 2][node + 1].location.append(self.point_lst[i][2])
-            print(f'hex {i} add {self.point_lst[i][2]} to {self.grid.node_list[top_row + 2][node + 1]}')
 
             if top_row < 4:
                 self.grid.node_list[top_row + 3][node + 1].location.append(self.point_lst[i][3])
-
             else:
                 self.grid.node_list[top_row + 3][node].location.append(self.point_lst[i][3])
 
-
-
-
-
+        # Tells all nodes to average out nodes
         for row in self.grid.node_list:
             for node in row:
                 node.avg_location()
 
-
+    # Prints all buildings to board
     def draw_building(self, screen):
         for row in self.grid.node_list:
             for node in row:
-                pygame.draw.polygon(screen, (100, 20, 50), self.__calculate_diamond(node.location[0], node.location[1], 10))
-
-
-
-
-
-
+                pygame.draw.polygon(screen, (100, 20, 50),
+                                    self.__calculate_diamond(node.location[0], node.location[1], 10))
