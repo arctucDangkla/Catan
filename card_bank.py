2ad78fcaf62513842c963c9b332b061134e194b6
riossa36 <riossa@mail.gvsu.edu>
2025-03-17 17:30:05 -0400

    Update card_bank.py

--- card_bank.py
+++ card_bank.py
@@ -1,4 +1,5 @@
 import pygame
+from button import Button
 
 class CardBank:
     # User can either be 'game' or 'player'
@@ -33,9 +34,6 @@ class CardBank:
             self.user = user.lower()
         else:
             raise ValueError('Invalid input')
-
-    def __str__(self):
-        return str(self.bank)
 
     # Num has a default value of 1, to remove just 1 card.
     # Card should match one of the keys from the dictionary.
@@ -83,4 +81,37 @@ class CardBank:
         else:
             self.bank[card] += num
 
+    # Draws an individual card as a Button
+    #sheep_button = button.Button(0, SCREENHEIGHT * .10, "images/resources_sheep.png", 0, 0, 0.2)
+    # origin width = 326 | scaled width = 65.2
+    # origin height = ??? | scaled height = 95.8
+    def draw_card(self, card, x, y, screen):
+        if card == 'Wo':
+            return Button(x, y, "images/resources_wood.png", 0, 0, 0.2)
+        elif card == 'O':
+            return Button(x, y, "images/resources_ore.png", 0, 0, 0.2)
+        elif card == 'S':
+            return Button(x, y, "images/resources_sheep.png", 0, 0, 0.2)
+        elif card == 'B':
+            return Button(x, y, "images/resources_brick.png", 0, 0, 0.2)
+        elif card == 'Wh':
+            return Button(x, y, "images/resources_wheat.png", 0, 0, 0.2)
+        else:
+            return Button(x, y, "images/devcard_back.png", 0, 0, 0.2)
+
+        # Draws the bank with all cards
+    def draw_bank(self, screen, screen_width):
+        # If the bank is of type game, draw the bank on right side
+        # If the bank is of type player, draw the bank of left side
+        # Make sure to display number of cards on the corresponding sides!!! #
+        resources = ['Wo','O', 'S', 'B', 'Wh', 'D']
+        if self.user == 'player':
+            x = 0
+        else:
+            x = screen_width - 66 # 65 is width of card
+        y = 0
+        for resource in resources:
+            temp = self.draw_card(resource, x, y, screen)
+            temp.draw(screen)
+            y += 100
 
